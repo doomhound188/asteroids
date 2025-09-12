f029ddcbe849a4006965b3d7a021e6b80e221fef
doomhound188 <doomhound188@proton.me>
2025-09-12 11:43:44 -0400

    Implement CircleShape and Player classes with drawing and updating functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT)
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,19 +9,23 @@ def main():
 
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
     while running:
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+            
+        player.update(dt)
 
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
